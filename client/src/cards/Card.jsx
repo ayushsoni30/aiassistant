63920e51ed55dd5ae85804e9db970a1312955d62
ayushsoni30 <ayushsoni55aa@gmail.com>
2026-07-19 11:51:58 +0530

    enhanced card

--- client/src/cards/Card.jsx
+++ client/src/cards/Card.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Card = ({image}) => {
   return (
-    <div className=" h-50 w-60  bg-black rounded-lg shadow-lg overflow-hidden border-2 border-blue-600">
-      <img src={image}  className=" h-50 w-60 object-cover" />
+    <div className=" h-70 w-70  bg-black rounded-lg shadow-lg overflow-hidden border-2 border-blue-600 hover:shadow-2xl hover:shadow-blue-400 cursor-pointer hover:border-amber-100">
+      <img src={image}  className=" h-full w-full object-cover" />
     </div>
   )
 }
