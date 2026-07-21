8378bd823952ac86491123dc0378dfa4eb40993d
ayushsoni30 <ayushsoni55aa@gmail.com>
2026-07-22 00:49:22 +0530

    added styles in customize page

--- client/src/cards/Card.jsx
+++ client/src/cards/Card.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-
-const Card = ({image}) => {
+const Card = ({ image }) => {
   return (
-    <div className=" h-70 w-70  bg-black rounded-lg shadow-lg overflow-hidden border-2 border-blue-600 hover:shadow-2xl hover:shadow-blue-400 cursor-pointer hover:border-amber-100">
-      <img src={image}  className=" h-full w-full object-cover" />
+    <div className="h-50 w-50 md:h-70 md:w-70 bg-black rounded-lg shadow-lg overflow-hidden border-2 border-blue-600 hover:shadow-2xl hover:shadow-blue-400 hover:border-amber-100 cursor-pointer transition-all">
+      <img
+        src={image}
+        alt="Card"
+        className="h-full w-full object-cover"
+      />
     </div>
   )
 }
--- client/src/pages/customize.jsx
+++ client/src/pages/customize.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import Card from '../cards/Card'
-import image from '../assets/image.png'
-
-import image3 from '../assets/image3.avif'
-
-import image5 from '../assets/image5.jpg'
+import React from "react";
+import Card from "../cards/Card";
+import { MdUpload } from "react-icons/md";
+import image3 from "../assets/image3.avif";
+import image5 from "../assets/image5.jpg";
 
 const Customize = () => {
   return (
-<div className="w-full h-screen bg-linear-to-t from-black to-blue-600 p-5 flex justify-center items-center  gap-4">
-      <Card image={image5} />
-      <Card image={image3} />
-       
-    </div>
-  )
-}
+    <div className="w-full min-h-screen bg-linear-to-t from-black to-blue-600 flex flex-col justify-center items-center p-5">
 
-export default Customize
+      {/* Heading */}
+      <h1 className="text-blue-300 text-4xl md:text-5xl font-bold mb-10 text-center">
+        Select your Avatar
+      </h1>
+
+     
+      <div className="flex flex-col md:flex-row justify-center items-center gap-5">
+        <Card image={image5} />
+
+        <Card image={image3} />
+
+        <div className="h-50 w-50 md:h-70 md:w-70 bg-black rounded-lg border-2 border-blue-600 flex flex-col justify-center items-center gap-4 cursor-pointer hover:border-white hover:shadow-2xl hover:shadow-blue-400 transition-all">
+          <MdUpload className="text-white h-8 w-8 md:h-12 md:w-12" />
+
+          <p className="text-white font-medium text-lg md:text-xl">
+            Upload Image
+          </p>
+        </div>
+        
+      </div>
+<button className=" mt-30 min-w-80 h-15 rounded-4xl text-3xl font-bold text-blue-300 border-2 border-blue-500 ">Next</button>
+    </div>
+  );
+};
+
+export default Customize;
